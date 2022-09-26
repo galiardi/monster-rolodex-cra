3eb1bfe9637d586bf38153b5c30ce6b56da71874
galiardi <galiardi.dev@gmail.com>
2022-09-26 17:43:09 -0300

    Delete SearchInput/onStringChange from App

--- src/App.js
+++ src/App.js
@@ -5,14 +5,11 @@ import SearchInput from "./components/search-input/search-input.component";
 import "./App.css";
 
 const App = () => {
-  console.log("render App");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonster] = useState([]);
   const [searchString, setSearchString] = useState("");
-  const [string, setString] = useState("");
 
   useEffect(() => {
-    console.log("fetching");
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
       .then((users) => setMonsters(users));
@@ -20,7 +17,6 @@ const App = () => {
 
   useEffect(() => {
     const newFilteredMonsters = monsters.filter((monster) => {
-      console.log("filtering monster");
       return monster.name.toLowerCase().includes(searchString);
     });
     setFilteredMonster(newFilteredMonsters);
@@ -31,21 +27,11 @@ const App = () => {
     setSearchString(newSearchString);
   };
 
-  const onStringChange = (event) => {
-    const newString = event.target.value;
-    setString(newString);
-  };
-
   return (
     <div>
       <h1 className="app-title">Monster Rolodex</h1>
       <SearchInput placeholder="..." onChangeHandler={onSearchChange} />
       <CardList monsters={filteredMonsters} />
-      <SearchInput
-        value={string}
-        placeholder="..."
-        onChangeHandler={onStringChange}
-      />
     </div>
   );
 };
