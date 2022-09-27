88682b17f124286ee8acb91336383a1acf09defe
galiardi <galiardi.dev@gmail.com>
2022-09-27 15:32:18 -0300

    Small changes in CardList and SearchInput

--- src/components/card-list/card-list.component.js
+++ src/components/card-list/card-list.component.js
@@ -2,9 +2,9 @@ import Card from "../card/card.component";
 
 import "./card-list.styles.css";
 
-const CardList = ({ className, monsters }) => {
+const CardList = ({ monsters }) => {
   return (
-    <div className={`card-list ${className}`}>
+    <div className={`card-list`}>
       {monsters.map((monster) => {
         return <Card monster={monster} key={monster.id} />;
       })}
--- src/components/search-input/search-input.component.js
+++ src/components/search-input/search-input.component.js
@@ -1,14 +1,12 @@
 import "./search-input.styles.css";
 
 const SearchInput = ({ className, placeholder, onChangeHandler }) => {
-  return (
-    <input
-      className={`search-box ${className}`}
-      type="search"
-      placeholder={placeholder}
-      onChange={onChangeHandler}
-    />
-  );
+  <input
+    className={`search-box ${className}`}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />;
 };
 
 export default SearchInput;
